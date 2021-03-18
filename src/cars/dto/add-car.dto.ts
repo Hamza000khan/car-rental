58be292ba073f97ca4de29cd144b1d99a98b5889
Hamza khan <khanhamza124@gmail.com>
2021-03-18 18:40:10 +0530

    cars dto for swagger property

--- src/cars/dto/add-car.dto.ts
+++ src/cars/dto/add-car.dto.ts
@@ -1,24 +1,32 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsDate } from 'class-validator';
 
 export class AddCarDto {
   @IsNotEmpty()
+  @ApiProperty({ type: String, description: 'License Number' })
   carLicenseNumber: string;
 
   @IsNotEmpty()
+  @ApiProperty({ type: String, description: 'Manufacturer' })
   Manufacturer: string;
 
   @IsNotEmpty()
+  @ApiProperty({ type: String, description: 'Car Model' })
   Model: string;
 
   @IsNotEmpty()
+  @ApiProperty({ type: String, description: 'City' })
   city: string;
 
   @IsNotEmpty()
+  @ApiProperty({ type: Number, description: 'Base Price' })
   basePrice: number;
 
   @IsNotEmpty()
+  @ApiProperty({ type: Number, description: 'Price Per Hour' })
   pricePerHour: number;
 
   @IsNotEmpty()
+  @ApiProperty({ type: Number, description: 'Security Deposit' })
   securityDeposit: number;
 }
--- src/cars/dto/get-cars-filter.dto.ts
+++ src/cars/dto/get-cars-filter.dto.ts
@@ -1,7 +1,9 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { isIn, IsOptional } from 'class-validator';
 import { CarStatus } from '../car-status.enum';
 
 export class getCarsFilterDto {
   @IsOptional()
+  @ApiProperty({ type: String, description: 'Car Status' })
   status: CarStatus;
 }
